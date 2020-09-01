1061dd3d795a2e3e936425cace205ba7a872036e
zabdi <zjvalenciana@miners.utep.edu>
2020-09-01 16:12:28 -0600

    file checking

--- WordCount.py
+++ WordCount.py
@@ -1,1 +1,9 @@
-print("hello")
+from sys import argv
+from os.path import exists
+
+script, inout_file, output_file = argv
+
+print(f"Dies input file exist? {exists(input_file)}")
+print(f"Does output file exist? {exists(output_file)}")
+print("hit RETURN to continue, CTRL-C to abort.")
+input()
