c7d0ed47d7d285c273fcdd7fd789bf3116778388
zabdi <zjvalenciana@miners.utep.edu>
2020-09-01 17:03:55 -0600

    worknig word count python program

--- WordCount.py
+++ WordCount.py
@@ -23,3 +23,14 @@ word_list = file_data.split() #store in a list
 dictionary = {}
 for word in word_list:
     dictionary[word] = dictionary.get(word, 0) + 1 #add words and increment values
+
+out_file = open(output_file, 'w') #open file
+out_file.truncate() #delete everything
+
+for ele in sorted(dictionary):
+    string = str(dictionary[ele]) + "\t" + str(ele)
+    out_file.write(string)
+    out_file.write("\n")
+
+out_file.close()
+file_copy.close()
